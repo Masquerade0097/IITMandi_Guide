2a5109e402654faa4d83458d18a66b059d857451
Masquerade <masquerade0097@gmail.com>
2017-07-04 23:53:17 +0530

    hdpi icons updated

--- IITMandi/app/src/main/java/com/example/priyanshu/iitmandi/OverviewActivity.java
+++ IITMandi/app/src/main/java/com/example/priyanshu/iitmandi/OverviewActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import static com.example.priyanshu.iitmandi.R.id.directorCard;
-
 /**
  * Created by priyanshu on 4/7/17.
  */
